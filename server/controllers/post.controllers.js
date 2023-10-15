e0cac0b98fcca3274bec52acc59afe50bc29fca0
Krishna Paul <krishnapaulmailbox@gmail.com>
2023-10-16 00:52:56 +0530

    feed posts api update

--- server/controllers/post.controllers.js
+++ server/controllers/post.controllers.js
@@ -170,15 +170,15 @@ const getFeedPosts = async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
     const following = user.following;
-    const feedPosts = await Post.find({ postedBy: { $in: [...following, userId] } }).sort({
-      createdAt: -1,
-    });
-    if (feedPosts.length === 0) {
+    // const feedPosts = await Post.find({ postedBy: { $in: [...following, userId] } }).sort({
+    //   createdAt: -1,
+    // });
+    // if (feedPosts.length === 0) {
 		const allPosts = await Post.find({}).sort({ createdAt: -1 });
 		res.status(200).json({ feedPosts: allPosts });
-    } else {
-      res.status(200).json({ feedPosts });
-    }
+    // } else {
+    //   res.status(200).json({ feedPosts });
+    // }
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
